a85edc6bac667b2ffa065b3d866a4aa7d58a363c
Gartorware <gartorware@gmail.com>
2020-03-26 12:04:10 +0100

    (fix) Compilation error

--- projects/ionic-privacy-consent/src/public-api.ts
+++ projects/ionic-privacy-consent/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/ionic-privacy-consent.module';
 export * from './lib/providers/ionic-privacy-consent';
 export * from './lib/components/ionic-privacy-consent/ionic-privacy-consent-component';
+export * from './lib/components/ionic-privacy-consent-manage/ionic-privacy-consent-manage-component';
